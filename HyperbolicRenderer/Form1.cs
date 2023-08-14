a5addd4da953469a54a70978edd6856a381d1dee
Chris <ccw100asdf@gmail.com>
2023-08-14 12:34:52 +1000

    Added a deformed grid pattern

--- HyperbolicRenderer/Form1.cs
+++ HyperbolicRenderer/Form1.cs
@@ -23,7 +23,7 @@ namespace HyperbolicRenderer
             }
             m = new Map(sides, pictureBox1.Width / 2f);
             m.GenerateShape();
-            m.GenerateVolume(0.775f/2);
+            m.GenerateVolume(0.775f);
             pictureBox1.Invalidate();
         }
 
@@ -35,14 +35,27 @@ namespace HyperbolicRenderer
                 return;
             }
 
-            e.Graphics.FillPolygon(new Pen(Color.Green).Brush, m.points);
+            e.Graphics.FillPolygon(new Pen(Color.DarkBlue).Brush, m.points);
             foreach (var trapezium in m.volume)
             {
-                e.Graphics.DrawPolygon(new Pen(Color.Blue), new PointF[4] { trapezium.top_left, trapezium.bottom_left, trapezium.bottom_right, trapezium.top_right });
+                e.Graphics.DrawPolygon(new Pen(Color.White), new PointF[4] { trapezium.top_left, trapezium.bottom_left, trapezium.bottom_right, trapezium.top_right });
             }
             foreach(var connection in m.connections)
             {
-              //  e.Graphics.FillEllipse(new Pen(Color.Red).Brush, connection.X-4, connection.Y-4, 8, 8);
+                //e.Graphics.FillEllipse(new Pen(Color.Red).Brush, connection.X-2, connection.Y-2, 4, 4);
+            }
+
+            using (var path = new System.Drawing.Drawing2D.GraphicsPath())
+            {
+                path.AddPolygon(m.points);
+
+                // Uncomment this to invert:
+                 path.AddRectangle(pictureBox1.ClientRectangle);
+
+                using (var brush = new SolidBrush(Color.Black))
+                {
+                    e.Graphics.FillPath(brush, path);
+                }
             }
         }
     }
@@ -104,20 +117,91 @@ namespace HyperbolicRenderer
             {
                 for (int x = 0; x < (volumewidth); ++x)
                 {
-                    float graphx = Math.Abs(relativeradius - x);
-                    float graphy = Math.Abs(relativeradius - y);
-                    float heightmod = (float)Math.Tanh(graphx/volumewidth);
+                    float x_scale = float.MaxValue;
+                    float y_scale = float.MaxValue;
+                    if (x==7)
+                    {
 
+                    }
+                    foreach (var point in points)
+                    {
+                        //Use herons formula to find the area of the bounded area of the triangle
+                        //basevector = point
+                        PointF a = new PointF((x*squaresize) - (radius), (y*squaresize) - (radius)); //From 0,0 to the current point
+                        PointF b = new PointF((point.X) - ((x*squaresize)),(point.Y) - ((y*squaresize))); //From point to the current point
+
+                        //Area = sqrt(s(s - a)(s - b)(s - c))
+                        //Where s = perimetre / 2
+
+                        float mag_a = (float)Math.Sqrt(a.X*a.X + a.Y*a.Y);
+                        float mag_c = radius;
+                        float mag_b = (float)Math.Sqrt(b.X*b.X + b.Y*b.Y);
+
+                        float s = (mag_c + mag_a + mag_b)/2;
+
+                        float area = (float)Math.Sqrt(s * (s-mag_a) * (s-mag_b) * (s-mag_c));
+
+                        //From the area the height can be determined as:
+                        //a = 1/2 * b * h
+                        //h = 2a / b
+
+                        float height = 2 * area / mag_c;
+                        //The height is the shortest distance and therefore the scaling factor
+                        if (mag_b < radius / 2)
+                        {
+                            height = mag_b;
+                        }
+                        else
+                        {
+                            height = (float)Math.Sqrt(mag_b * mag_b - (radius / 2) * (radius / 2));
+                        }
+                        //if (height > radius * 0.75f)
+                        //{
+                        //    height = radius * 0.75f;
+                        //}
+                        if (a.X > 0)
+                        {
+                            if (height < Math.Abs(x_scale))
+                            {
+                                x_scale = -height;
+                            }
+                        }
+                        else
+                        {
+                            if (height < Math.Abs(x_scale))
+                            {
+                                x_scale = height;
+                            }
+                        }
+                        if (a.Y > 0)
+                        {
+                            if (height < Math.Abs(y_scale))
+                            {
+                                y_scale = -height;
+                            }
+                        }
+                        else
+                        {
+                            if (height < Math.Abs(y_scale))
+                            {
+                                y_scale = height;
+                            }
+                        }
+                    }
+                    if (x_scale >= 100)
+                    {
+
+                    }
+                    x_scale = (float)Math.Tanh((x_scale) / 200)/2f;
+                    y_scale = (float)Math.Tanh((y_scale) / 200)/2f;
+                    if (x_scale >= 1)
+                    {
+
+                    }
                     float ay = y;
                     float ax = x;
-                    if (y > volumewidth/2)
-                    {   
-                        ay += heightmod;
-                    }
-                    else
-                    {
-                        ay -= heightmod;
-                    }
+                    ay += y_scale;
+                    ax += x_scale;
                     if (ay == float.NegativeInfinity)
                     {
 

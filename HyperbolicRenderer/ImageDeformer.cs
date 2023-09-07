15e30980b348b0e3200d15519f4a04d3054566f3
xeenoon <ccw100asdf@gmail.com>
2023-09-07 12:41:33 +1000

    optimizations

--- HyperbolicRenderer/ImageDeformer.cs
+++ HyperbolicRenderer/ImageDeformer.cs
@@ -9,6 +9,9 @@ using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using ManagedCuda;
+using ManagedCuda.BasicTypes;
+using ManagedCuda.VectorTypes;
 
 namespace HyperbolicRenderer
 {
@@ -22,19 +25,15 @@ namespace HyperbolicRenderer
             imagedata = GC_pacifier.LockBits(new Rectangle(0, 0, originalimage.Width, originalimage.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppPArgb);
         }
 
-        bool asyncrunning = false;
         public unsafe void DeformImageToPolygon(Func<Point, Point> DeformFunction, Point offset, Bitmap resultBitmap)
         {
-            while (asyncrunning) { }
-            asyncrunning = true;
-
             int width = imagedata.Width;
             int height = imagedata.Height;
 
             BitmapData outputData = resultBitmap.LockBits(new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppPArgb);
 
             const int sectionwidth = 2;
-            const int sectionradius = sectionwidth/2;
+            const int sectionradius = sectionwidth / 2;
 
             int numRows = ((height - sectionradius) / sectionwidth) + 2;
             int numCols = ((width - sectionradius) / sectionwidth) + 2; //Add 2 to store elements behind and infront
@@ -53,15 +52,10 @@ namespace HyperbolicRenderer
                     Point blockcentre = new Point((col * sectionwidth) - (sectionradius), (row * sectionwidth) - (sectionradius));
                     Point newtransform = DeformFunction(blockcentre);
 
-                    if ((newtransform.X == offset.X) || (newtransform.Y == offset.Y))
-                    {
-                    }
-
                     xCoordinates[index] = (int)newtransform.X;
                     yCoordinates[index] = (int)newtransform.Y;
                 }
             }
-
             // Now, use the precomputed deformation values
             for (int row = 1; row < numRows - 1; row++)
             {
@@ -96,140 +90,89 @@ namespace HyperbolicRenderer
                         continue;
                     }
                     //Instead of increasing the size of the area being drawn, scale the old image
-                    BitmapData sourceData = ResizeBitmap(imagedata, new Rectangle(blockcentrex, blockcentrey, sectionwidth, sectionwidth), finalxresolution, finalyresolution);
-                    CopyRectangles(sourceData,
-                                   outputData,
+                    var sourceData = ResizeBitmapFast((byte*)imagedata.Scan0.ToPointer(), imagedata.Stride, new Rectangle(blockcentrex, blockcentrey, sectionwidth, sectionwidth), finalxresolution, finalyresolution);
+                    CopyRectangles(sourceData, finalxresolution * 4,
+                                   (byte*)outputData.Scan0.ToPointer(), outputData.Stride,
                                    new Rectangle(0, 0, finalxresolution, finalyresolution),
                                    new Rectangle(newtransformx, newtransformy, finalxresolution, finalyresolution));
-                    Marshal.FreeHGlobal(sourceData.Scan0);
+                    Marshal.FreeHGlobal((IntPtr)sourceData);
                 }
             }
-
             Marshal.FreeHGlobal((IntPtr)xCoordinates);
             Marshal.FreeHGlobal((IntPtr)yCoordinates);
             resultBitmap.UnlockBits(outputData);
-            asyncrunning = false;
         }
-        void TestResize()
-        {
-            for (int i = 0; i < 1000; ++i)
-            {
-                var b = ResizeBitmap(imagedata, new Rectangle(0, 0, 4, 4), 2, 2);
-                Marshal.FreeHGlobal(b.Scan0);
-            }
-        }
-        public unsafe BitmapData ResizeBitmap(BitmapData sourceData, Rectangle areafrom, int newwidth, int newheight)
+        public unsafe byte* ResizeBitmapFast(byte* sourceData, int sourcestride, Rectangle areafrom, int newwidth, int newheight)
         {
             const int bytesPerPixel = 4;
+            int deststride = newwidth * bytesPerPixel;
 
-            // Lock the source and destination bitmaps in memory
-            BitmapData destData = new BitmapData
-            {
-                Width = newwidth,
-                Height = newheight,
-                Stride = newwidth * bytesPerPixel, // Assuming 32bpp ARGB format
-                PixelFormat = PixelFormat.Format32bppArgb
-            };
-            destData.Scan0 = Marshal.AllocHGlobal(destData.Stride * newheight);
-            
+            // Pointer to the first pixel of the source and destination bitmaps
+            byte* srcPointer = sourceData + (areafrom.X * bytesPerPixel) + (areafrom.Y * sourcestride);
+            byte* destPointer = (byte*)Marshal.AllocHGlobal(deststride * newheight);
             // Calculate the scaling factors for width and height
             float scaleX = (float)areafrom.Width / newwidth;
             float scaleY = (float)areafrom.Height / newheight;
 
             if (scaleX == 1 && scaleY == 1)
             {
-                CopyRectangles(sourceData, destData, areafrom, new Rectangle(0,0,newwidth, newheight));
-                return destData;
+                CopyRectangles(sourceData, sourcestride, destPointer, newwidth*bytesPerPixel, areafrom, new Rectangle(0, 0, newwidth, newheight));
+                return destPointer;
             }
-
-            // Pointer to the first pixel of the source and destination bitmaps
-            byte* srcPointer = (byte*)sourceData.Scan0 + (areafrom.X * bytesPerPixel) + (areafrom.Y * sourceData.Stride);
-            byte* destPointer = (byte*)destData.Scan0;
 
             for (int y = 0; y < newheight; y++)
-            {
+            {                
                 for (int x = 0; x < newwidth; x++)
                 {
-                    // Calculate the corresponding position in the source bitmap
-                    float srcX = x * scaleX;
-                    float srcY = y * scaleY;
+                    // Calculate the byte offsets for the source and destination pixels
+                    long srcOffset = (y * sourcestride) + (x * bytesPerPixel);
+                    long destOffset = (y * deststride) + (x * bytesPerPixel);
 
-                    // Find the neighboring pixels
-                    int x1 = (int)srcX;
-                    int x2 = x1 + 1;
-                    int y1 = (int)srcY;
-                    int y2 = y1 + 1;
+                    // Use MemoryCopy to copy the pixel data
+                    Buffer.MemoryCopy(srcPointer + srcOffset, destPointer + destOffset, bytesPerPixel, bytesPerPixel);
 
-                    // Ensure boundaries
-                    if (x2 >= sourceData.Width) x2 = x1;
-                    if (y2 >= sourceData.Height) y2 = y1;
-
-                    // Calculate interpolation weights
-                    float xWeight = srcX - x1;
-                    float yWeight = srcY - y1;
-
-                    // Calculate byte offsets for the source and destination pixels
-                    long srcOffset1 = (y1 * sourceData.Stride) + (x1 * bytesPerPixel);
-                    long srcOffset2 = (y1 * sourceData.Stride) + (x2 * bytesPerPixel);
-                    long srcOffset3 = (y2 * sourceData.Stride) + (x1 * bytesPerPixel);
-                    long srcOffset4 = (y2 * sourceData.Stride) + (x2 * bytesPerPixel);
-
-                    long destOffset = (y * destData.Stride) + (x * bytesPerPixel);
-
-                    // Interpolate each channel (RGBA)
-                    for (int i = 0; i < 4; i++)
-                    {
-                        float topInterpolation = (1 - xWeight) * srcPointer[srcOffset1 + i] + xWeight * srcPointer[srcOffset2 + i];
-                        float bottomInterpolation = (1 - xWeight) * srcPointer[srcOffset3 + i] + xWeight * srcPointer[srcOffset4 + i];
-                        destPointer[destOffset + i] = (byte)((1 - yWeight) * topInterpolation + yWeight * bottomInterpolation);
-                    }
                 }
             }
-            return destData;
+            return destPointer;
         }
-        private void CopyRectangles(BitmapData sourceData, BitmapData destinationData, Rectangle sourceRect, Rectangle destinationRect)
+        private unsafe void CopyRectangles(byte* sourcePtr, int sourceStride, byte* destinationPtr, int destinationStride, Rectangle sourceRect, Rectangle destinationRect)
         {
-            unsafe
+            const int bytesPerPixel = 4;
+
+            int sourceStartY = sourceRect.Y;
+            int sourceStartX = sourceRect.X * bytesPerPixel;
+            int sourceWidthInBytes = sourceRect.Width * bytesPerPixel;
+            int sourceHeight = sourceRect.Height;
+
+            int destinationStartY = destinationRect.Y;
+            int destinationStartX = destinationRect.X * bytesPerPixel;
+            int destinationHeight = destinationRect.Height;
+
+            // Ensure the source and destination rectangles have the same width
+            if (sourceWidthInBytes != destinationRect.Width * bytesPerPixel)
             {
-                const int bytesPerPixel = 4;
-
-                int sourceStride = sourceData.Stride;
-                int destinationStride = destinationData.Stride;
-
-                byte* sourcePtr = (byte*)sourceData.Scan0.ToPointer();
-                byte* destinationPtr = (byte*)destinationData.Scan0.ToPointer();
-
-                int sourceStartY = sourceRect.Y;
-                int sourceStartX = sourceRect.X * bytesPerPixel;
-                int sourceWidthInBytes = sourceRect.Width * bytesPerPixel;
-                int sourceHeight = sourceRect.Height;
-
-                int destinationStartY = destinationRect.Y;
-                int destinationStartX = destinationRect.X * bytesPerPixel;
-                int destinationHeight = destinationRect.Height;
-
-                // Ensure the source and destination rectangles have the same width
-                if (sourceWidthInBytes != destinationRect.Width * bytesPerPixel)
-                {
-                    throw new ArgumentException("Source and destination rectangles must have the same width");
-                }
-
-                // Loop through each row in the source and destination rectangles
-                for (int y = 0; y < sourceHeight && y < destinationHeight; ++y)
-                {
-                    int sourceOffset = ((sourceStartY + y) * sourceStride) + sourceStartX;
-                    int destinationOffset = ((destinationStartY + y) * destinationStride) + destinationStartX;
-
-                    // Calculate the number of bytes to copy for this row
-                    int bytesToCopy = (destinationRect.Width * bytesPerPixel);
-
-                    // Use Buffer.MemoryCopy to copy the data for this row
-                    Buffer.MemoryCopy(sourcePtr + sourceOffset,
-                        destinationPtr + destinationOffset,
-                        bytesToCopy,
-                        bytesToCopy);
-                }
+                throw new ArgumentException("Source and destination rectangles must have the same width");
             }
+
+            // Loop through each row in the source and destination rectangles
+            for (int y = 0; y < sourceHeight && y < destinationHeight; ++y)
+            {
+                int sourceOffset = ((sourceStartY + y) * sourceStride) + sourceStartX;
+                int destinationOffset = ((destinationStartY + y) * destinationStride) + destinationStartX;
+
+                // Calculate the number of bytes to copy for this row
+                int bytesToCopy = (destinationRect.Width * bytesPerPixel);
+
+                // Use Buffer.MemoryCopy to copy the data for this row
+                Buffer.MemoryCopy(sourcePtr + sourceOffset,
+                    destinationPtr + destinationOffset,
+                    bytesToCopy,
+                    bytesToCopy);
+            }
+        }
+        public unsafe void CopyRectangles(BitmapData input, BitmapData output, Rectangle sourcerectangle, Rectangle destinationRect)
+        {
+            CopyRectangles((byte*)input.Scan0.ToPointer(), input.Stride, (byte*)output.Scan0.ToPointer(), output.Stride, sourcerectangle, destinationRect);
         }
     }
 }

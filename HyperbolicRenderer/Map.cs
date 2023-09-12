e1da4f2bf5db54bbcca1e8c607ff7064c7c085d8
xeenoon <ccw100asdf@gmail.com>
2023-09-12 22:44:08 +1000

    Refactored code

--- HyperbolicRenderer/Map.cs
+++ HyperbolicRenderer/Map.cs
@@ -291,43 +291,4 @@ namespace HyperbolicRenderer
             return height;
         }
     }
-    public class TimeScalar
-    {
-        PointF centre;
-        System.Timers.Timer timer = new System.Timers.Timer();
-
-        public double time = 0;
-        public double period = 2;
-        public double amplitude = 0.05;
-        public double speed = 1;
-        public TimeScalar(PointF centre, bool usetimer = true)
-        {
-            this.centre = centre;
-            if (usetimer) 
-            {
-                timer.Interval = 100;
-                timer.Elapsed += new System.Timers.ElapsedEventHandler(Update);
-                timer.Start();
-            }
-        }
-        public void Update(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            time += 0.5f;
-        }
-        public System.Drawing.Point TransformPoint(System.Drawing.Point input)
-        {
-            PointF adjustedpoint = new PointF((input.X - centre.X), (input.Y - centre.Y));
-
-            //Based on time, points will be scaled based on their angle to the centre
-            double angle = Math.Atan(adjustedpoint.Y / adjustedpoint.X) + Math.PI/2;
-            float heightmultiplier = (float)((Math.Cos((angle * period * 2) + (time * speed))) * amplitude) + 1;
-            adjustedpoint.X *= heightmultiplier;    
-            adjustedpoint.Y *= heightmultiplier;
-
-            adjustedpoint.X += centre.X;
-            adjustedpoint.Y += centre.Y;
-
-            return new System.Drawing.Point((int)adjustedpoint.X, (int)adjustedpoint.Y);
-        }
-    }
 }
